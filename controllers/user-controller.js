1666bbbcc0c780283a141f8c330c3f1947129166
hpurring <hilarypurrington@gmail.com>
2022-02-03 21:56:36 -0500

    user and thought routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,7 +3,7 @@ const { User } = require('../models');
 const userController = {
 
     // find all users
-    getAllUser(req, res) {
+    getAllUsers(req, res) {
         User.find({})
         .populate({
             path: 'thoughts',
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const { createThought, deleteThought, addReaction, removeReaction } = require('../../controllers/thought-controller');
+
+// /api/thoughts/<userId>
+router.route('/:userId').post(createThought);
+
+// /api/thoughts/<userId>/<thoughtId>
+router
+    .route('/:userId/:thoughtId')
+    .put(addReaction)
+    .delete(deleteThought);
+
+router.route('/:userId/:thoughtId/:reactionId').delete(removeReaction);
+
+module.exports = router;
